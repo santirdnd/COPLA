6b89285ef16d721d93d3c84f3645e76455ba8621
santirdnd <santirdnd@gmail.com>
2021-06-30 10:57:12 +0200

    Fix too long wildcard expansion
    Depending on the configured path, wildcard expansion may result in too long a parameter list. Thanks to Caison Warmer for reporting the bug and providing the fix.

--- bin/download_Copla_databases.sh
+++ bin/download_Copla_databases.sh
@@ -5,6 +5,6 @@ tar -xf Copla_databases_latest.tar
 
 COPLA_DB_DIR=`grep '^COPLA_DB_DIR' copla.ini | cut -f2`
 
-gunzip ${COPLA_DB_DIR}/*.fna.gz
+find ${COPLA_DB_DIR} -type f -name "*.fna.gz" -print0 | xargs -0 gunzip
 
 awk -v path=${PWD}/${COPLA_DB_DIR} '{ print path "/" $1 ".fna" }' ${COPLA_DB_DIR}/CoplaDB.lst > ${COPLA_DB_DIR}/CoplaDB.fofn
